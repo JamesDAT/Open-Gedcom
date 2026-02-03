95912145da8c639121c512e0a18dc5f1e6026918
JamesDAT <JamesReHayden@gmail.com>
2026-02-04 03:52:42 +1100

    Added generated gedcom 7 spec test file

--- tests/DocumentTest.cpp
+++ tests/DocumentTest.cpp
@@ -4,22 +4,19 @@
 
 namespace OpenGedcom {
     void DocumentTest::TestDocument() {
-        Document doc = Document::ParseFile("tests/Samples/555SAMPLE.ged");
+        Document doc = Document::ParseFile("tests/Samples/spec7Generated.ged");
         auto& storage = doc.Storage();
         auto& graph = storage.Graph();
 
-        std::cout << "Graph Size: " << graph.size();
-        
-        if(auto individual = doc.Individual(1)) {
-            std::cout << "Has Indi 1\n";
-        }
-        else {
-            std::cout << "Not has Indi1\n";
+        std::cout << "Graph Size: " << graph.size() << '\n';
+
+        for(auto& record : graph) {
+            PrintChildren(record.get());
         }
     }
 
     void DocumentTest::PrintChildren(GedcomTag* tag) {
-        //std::cout << tag->RawValue() << '\n';
+        std::cout << tag->Type() << ' ' << tag->RawValue() << '\n';
         for(auto& child : tag->Children()) {
             PrintChildren(child.get());
         }
